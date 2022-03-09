3c33ec499c88ebb41dcbba49d65ea348a42cf0cc
Andhika Fajar K <andhikafajark@192.168.1.8>
2022-03-09 20:44:59 +0700

    Initial Commit

---
+++ go.mod
@@ -0,0 +1,5 @@
+module github.com/andhikafajark/golang-module.git
+
+go 1.17
+
+require github.com/andhikafajark/golang-say-hello v1.5.0
---
+++ main.go
@@ -0,0 +1,10 @@
+package main
+
+import (
+	"fmt"
+	golang_say_hello "github.com/andhikafajark/golang-say-hello"
+)
+
+func main() {
+	fmt.Println(golang_say_hello.SayHello())
+}
